0fcc8deabec569019125d038a86b0a37e06f7a77
CTKlab <hou1830@gmail.com>
2024-06-07 12:48:02 +0800

    Update Function_scatter_plot.R

--- R/Function_scatter_plot.R
+++ R/Function_scatter_plot.R
@@ -56,126 +56,127 @@ ctk.scatter <- function(
     stop("This function requires ggplot2 version 3.5.0 or higher.")
   }
 
-    # Subset data
-    df_sub <- select(df, all_of(c(var_x, var_y)))
+  # Subset data
+  df_sub <- select(df, all_of(c(var_x, var_y)))
 
-    # Remove NA rows
-    df_sub <- na.omit(df_sub)
+  # Remove NA rows
+  df_sub <- na.omit(df_sub)
 
-    # Calculate parameter
-    if (is.null(x_limits)) {
-      x_limits = c(min(df_sub[[var_x]]), max(df_sub[[var_x]]))
+  # Calculate parameter
+  if (is.null(x_limits)) {
+    x_limits = c(min(df_sub[[var_x]]), max(df_sub[[var_x]]))
+  }
+
+  if (is.null(y_limits)) {
+    y_limits = c(min(df_sub[[var_y]]), max(df_sub[[var_y]]))
+  }
+
+  if (is.null(legend_position)) {
+    legend_position = c(
+      min(df_sub[[var_x]]),
+      max(df_sub[[var_y]])
+    )
+  }
+
+  # Scatter Plot
+  p <- df_sub %>%
+    ggplot(aes(x = !!sym(var_x), y = !!sym(var_y))) +
+    geom_point(size = pt_size) +
+    scale_x_continuous(limits = x_limits) +
+    scale_y_continuous(limits = y_limits)
+
+  # Add minor ticks
+  suppressMessages(
+    if (!isFALSE(minor_ticks)) {
+      x_ticks <- pretty(x_limits)
+      x_interval <- diff(x_ticks)[1]
+      y_ticks <- pretty(y_limits)
+      y_interval <- diff(y_ticks)[1]
+      p <- p +
+        guides(x = guide_axis(minor.ticks = T), y = guide_axis(minor.ticks = T)) +
+        scale_x_continuous(breaks = x_ticks, minor_breaks = breaks_width(x_interval/(1 + minor_ticks[1]))) +
+        scale_y_continuous(breaks = y_ticks, minor_breaks = breaks_width(y_interval/(1 + minor_ticks[2])))
     }
+  )
 
-    if (is.null(y_limits)) {
-      y_limits = c(min(df_sub[[var_y]]), max(df_sub[[var_y]]))
-    }
+  # Correlation test
+  if (!isFALSE(cor)) {
 
-    if (is.null(legend_position)) {
-      legend_position = c(
-        min(df_sub[[var_x]]),
-        max(df_sub[[var_y]])
+    # Fit model
+    var_x <- make.names(var_x)
+    var_y <- make.names(var_y)
+    fmla <- reformulate(termlabels = var_y, response = var_x)
+    if (cor == "lm") fit <- lm(fmla, data = df_sub)
+
+    # Extract slope and intercept
+    slope <- coef(fit)[2]
+    intercept <- coef(fit)[1]
+
+    # Coefficients
+    correlation <- cor.test(df_sub[[var_x]], df_sub[[var_y]], method = cor_method)
+    r_value <- correlation$estimate
+    p_value <- correlation$p.value
+
+    # Create annotation text
+    regression_eq <- paste0("y = ", round(slope, 2), "x + ", round(intercept, 2))
+    r_text <- paste0("r = ", round(r_value, 2))
+    p_text <- paste0("p-value = ", format.pval(p_value, digits = 3))
+    annotation <- paste(regression_eq, r_text, p_text, sep = "\n")
+
+    # Add to plot
+    p <- p +
+      geom_smooth(
+        formula = y ~ x,
+        method = "lm",
+        color = cor_line_col,
+        se = cor_se,
+        fill = cor_se_col) +
+      annotate(
+        "text",
+        x = legend_position[1],
+        y = legend_position[2],
+        label = annotation,
+        hjust = "inward",
+        vjust = "inward"
       )
-    }
+  }
 
-    # Scatter Plot
-    p <- df_sub %>%
-      ggplot(aes_string(x = var_x, y = var_y)) +
-      geom_point(size = pt_size) +
-      scale_x_continuous(limits = x_limits) +
-      scale_y_continuous(limits = y_limits)
+  # Add titles and labels
+  if (!is.null(x_axis_label)) {
+    p <- p + labs(x = x_axis_label)
+  }
 
-    # Add minor ticks
-    suppressMessages(
-      if (!isFALSE(minor_ticks)) {
-        x_ticks <- pretty(x_limits)
-        x_interval <- diff(x_ticks)[1]
-        y_ticks <- pretty(y_limits)
-        y_interval <- diff(y_ticks)[1]
+  if (!is.null(y_axis_label)) {
+    p <- p + labs(y = y_axis_label)
+  }
 
-        p <- p +
-          guides(x = guide_axis(minor.ticks = T), y = guide_axis(minor.ticks = T)) +
-          scale_x_continuous(breaks = x_ticks, minor_breaks = breaks_width(x_interval/(1 + minor_ticks[1]))) +
-          scale_y_continuous(breaks = y_ticks, minor_breaks = breaks_width(y_interval/(1 + minor_ticks[2])))
-      }
+  if (!is.null(title)) {
+    p <- p + labs(title = title)
+  }
+
+  if (!is.null(subtitle)) {
+    p <- p + labs(subtitle = subtitle)
+  }
+
+  # Set theme
+  p <- p +
+    scale_color_prism("floral") +
+    scale_fill_prism("floral") +
+    theme_prism(base_size = 12) +
+    theme(
+      plot.margin = unit(c(10, 10, 10, 10), "mm"),
+      axis.ticks.length = unit(5, "pt"),
+      axis.minor.ticks.length = rel(0.5)
     )
 
-    # Correlation test
-    if (!isFALSE(cor)) {
-      # Fit model
-      var_x <- make.names(var_x)
-      var_y <- make.names(var_y)
-      fmla <- reformulate(termlabels = var_y, response = var_x)
-      if (cor == "lm") fit <- lm(fmla, data = df_sub)
+  if (!is.null(title)) {
+    p <- p + theme(plot.title = element_text(hjust = 0))
+  }
 
-      # Extract slope and intercept
-      slope <- coef(fit)[2]
-      intercept <- coef(fit)[1]
+  if (!is.null(subtitle)) {
+    p <- p + theme(plot.subtitle = element_text(hjust = 0))
+  }
 
-      # Coefficients
-      correlation <- cor.test(df_sub[[var_x]], df_sub[[var_y]], method = cor_method)
-      r_value <- correlation$estimate
-      p_value <- correlation$p.value
+  return(p)
 
-      # Create annotation text
-      regression_eq <- paste0("y = ", round(slope, 2), "x + ", round(intercept, 2))
-      r_text <- paste0("r = ", round(r_value, 2))
-      p_text <- paste0("p-value = ", format.pval(p_value, digits = 3))
-      annotation <- paste(regression_eq, r_text, p_text, sep = "\n")
-
-      # Add to plot
-      p <- p +
-        geom_smooth(
-          formula = y ~ x,
-          method = "lm",
-          color = cor_line_col,
-          se = cor_se,
-          fill = cor_se_col) +
-        annotate(
-          "text",
-          x = legend_position[1],
-          y = legend_position[2],
-          label = annotation,
-          hjust = "inward",
-          vjust = "inward"
-      )
-    }
-
-    # Add titles and labels
-    if (!is.null(x_axis_label)) {
-      p <- p + labs(x = x_axis_label)
-    }
-
-    if (!is.null(y_axis_label)) {
-      p <- p + labs(y = y_axis_label)
-    }
-
-    if (!is.null(title)) {
-      p <- p + labs(title = title)
-    }
-
-    if (!is.null(subtitle)) {
-      p <- p + labs(subtitle = subtitle)
-    }
-
-    # Set theme
-    p <- p +
-      scale_color_prism("floral") +
-      scale_fill_prism("floral") +
-      theme_prism(base_size = 12) +
-      theme(
-        plot.margin = unit(c(10, 10, 10, 10), "mm"),
-        axis.ticks.length = unit(5, "pt"),
-        axis.minor.ticks.length = rel(0.5)
-      )
-
-    if (!is.null(title)) {
-      p <- p + theme(plot.title = element_text(hjust = 0))
-    }
-
-    if (!is.null(subtitle)) {
-      p <- p + theme(plot.subtitle = element_text(hjust = 0))
-    }
-
-    return(p)
 }
